616d45a231604edf8b34c9a61524a9c36ff9349c
Oleg Lobach <oleg@nanosemantics.ru>
2013-04-16 17:36:46 +0400

    воркэраунд ошибки при запуске монги

--- etc/puppet/manifests/mongo.pp
+++ etc/puppet/manifests/mongo.pp
@@ -1,6 +1,12 @@
 class mongo {
     include mongodb
 
+    # Это нужно только для обхода ошибки старта сервиса mongodb при отсутствии init-скрипта
+    exec {"mongodb-init":
+        command => "touch /etc/init.d/mongodb && chmod 755 /etc/init.d/mongodb",
+        before => Service["${::mongodb::params::old_servicename}"],
+    }
+
     mongodb::mongod {
         "mdb01":
             mongod_instance => "mdb01"
