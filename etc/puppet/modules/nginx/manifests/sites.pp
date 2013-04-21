6455b6a96e19c77e02075d27e91c752a7cccb4be
Oleg Lobach <oleg@lobach.info>
2013-04-21 19:04:38 +0400

    теперь гарантированно удаляется дефолтный сайт в нжинксе

--- etc/puppet/modules/nginx/manifests/sites.pp
+++ etc/puppet/modules/nginx/manifests/sites.pp
@@ -5,14 +5,18 @@ class nginx::sites {
                 exec { "/bin/ln -s /etc/nginx/sites-available/$name /etc/nginx/sites-enabled/$name":
                     unless => "/bin/sh -c '[ -L /etc/nginx/sites-enabled/$name ] \\
                                 && [ /etc/nginx/sites-enabled/$name -ef /etc/nginx/sites-available/$name ]'",
-                    notify => Service["nginx"],
-                    require => File["/etc/nginx/sites-available/$name"],
+                    before => Service["nginx"],
+                    require => [
+                        File["/etc/nginx/sites-available/$name"],
+                        Package["nginx"],
+                    ],
                 }
             }
             'absent' : {
                 exec { "/bin/rm /etc/nginx/sites-enabled/$name":
                     onlyif => "/usr/bin/test -h /etc/nginx/sites-enabled/$name",
-                    notify => Service["nginx"],
+                    before => Service["nginx"],
+                    require => Package["nginx"],
                 }
             }
             default: { err ( "Unknown ensure value: '$ensure'" ) }
